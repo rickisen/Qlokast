9acb308726ea9a4806573fab0b0c93e030ffc04f
rickisen <rickard.spam@gmail.com>
2016-02-29 15:08:29 +0100

    should render comments

--- public/wp-content/themes/twentysixteen-child/single-assignments.php
+++ public/wp-content/themes/twentysixteen-child/single-assignments.php
@@ -23,6 +23,13 @@ get_header(); ?>
             <?php the_content(); ?>
           </div>
 
+          <?php 
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) {
+              comments_template();
+            } 
+          ?>
+
 				</main><!-- #main -->
 			</div><!-- #primary -->
 		</div><!--col-->
