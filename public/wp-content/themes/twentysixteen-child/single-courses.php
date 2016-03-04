245d0869b47c73842a7cd470315fe31b86682124
rickisen <rickard.spam@gmail.com>
2016-03-04 14:09:59 +0100

    removed bootsrap

--- public/wp-content/themes/twentysixteen-child/single-courses.php
+++ public/wp-content/themes/twentysixteen-child/single-courses.php
@@ -6,79 +6,72 @@
  *
  * @package Qlokast
  */
-
 get_header(); ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-10 col-lg-offset-1 text-center">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
 
-					<?php the_post(); ?>
+    <?php the_post(); ?>
 
-          <div class="course">	
-            <h1><?php the_title(); ?></h1>
-            <p>Publicerad <?php the_time("Y-m-d H:i"); ?> av <?php the_author_posts_link(); ?></p>
-            <?php the_content(); ?>
-          </div>
+    <div class="course">	
+      <h1><?php the_title(); ?></h1>
+      <p>Publicerad <?php the_time("Y-m-d H:i"); ?> av <?php the_author_posts_link(); ?></p>
+      <?php the_content(); ?>
+    </div>
 
-          <!-- look for lessions conencted to this course -->
-          <?php
-            $args = array(
-              'meta_key'   => '_course_parrent',
-              'meta_value' => $post->ID,
-              'post_type'  => 'lession'
-            );
-            $the_query = new WP_Query( $args );
-          ?>
+    <!-- look for lessions conencted to this course -->
+    <?php
+      $args = array(
+        'meta_key'   => '_course_parrent',
+        'meta_value' => $post->ID,
+        'post_type'  => 'lession'
+      );
+      $the_query = new WP_Query( $args );
+    ?>
 
-          <?php if ( $the_query->have_posts() ) : ?>
-            <hr>
-            <h3> Lektioner </h3>
+    <?php if ( $the_query->have_posts() ) : ?>
+      <hr>
+      <h3> Lektioner </h3>
 
-            <!-- a new the loop with lessions -->
-            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-              <a href="<?php the_permalink() ?>"><h5><?php the_title(); ?></h5></a>
-            <?php endwhile; ?>
-            <br>
-            <?php wp_reset_postdata(); ?>
-          <?php endif; ?>
+      <!-- a new the loop with lessions -->
+      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <a href="<?php the_permalink() ?>"><h5><?php the_title(); ?></h5></a>
+      <?php endwhile; ?>
+      <br>
+      <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
 
-          <!-- a new the loop with lessions -->
-          <?php
-            $args = array(
-              'meta_key'   => '_lession_course_parrent',
-              'meta_value' => $post->ID,
-              'post_type'  => 'assignments'
-            );
-            $the_query = new WP_Query( $args );
-          ?>
+    <!-- a new the loop with lessions -->
+    <?php
+      $args = array(
+        'meta_key'   => '_lession_course_parrent',
+        'meta_value' => $post->ID,
+        'post_type'  => 'assignments'
+      );
+      $the_query = new WP_Query( $args );
+    ?>
 
-          <?php if ( $the_query->have_posts() ) : ?>
-            <hr>
-            <h3> Kurs Övningar </h3>
+    <?php if ( $the_query->have_posts() ) : ?>
+      <hr>
+      <h3> Kurs Övningar </h3>
 
-            <!-- a new the loop with lessions -->
-            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-              <a href="<?php the_permalink() ?>"><h5><?php the_title(); ?></h5></a>
-            <?php endwhile; ?>
+      <!-- a new the loop with lessions -->
+      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <a href="<?php the_permalink() ?>"><h5><?php the_title(); ?></h5></a>
+      <?php endwhile; ?>
 
-            <?php wp_reset_postdata(); ?>
-          <?php endif; ?>
+      <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
 
-          <?php 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) {
-              comments_template();
-            } 
-          ?>
+    <?php 
+      // If comments are open or we have at least one comment, load up the comment template.
+      if ( comments_open() || get_comments_number() ) {
+        comments_template();
+      } 
+    ?>
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
-		</div><!--col-->
-	</div><!--row-->
-</div><!--container-->
+  </main><!-- #main -->
+</div><!-- #primary -->
 
 
 <?php
