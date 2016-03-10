82fefd0369a14bf713f8cfe145db3ceed669bb45
rickisen <rickard.spam@gmail.com>
2016-03-10 23:36:27 +0100

    changed so that we manage speciall caps instead

--- public/wp-content/plugins/Qlokast School Class/functions.php
+++ public/wp-content/plugins/Qlokast School Class/functions.php
@@ -17,23 +17,52 @@ function schoolclass(){
   }
 
   // Handle incomming POSTS 
-  if (isset($_POST['_schoolclass']) && !empty($_POST['_schoolclass']) && isset($_POST['users']) ){
-    $newClass = $_POST['_schoolclass'];
-    $users = $_POST['users'];
+  if (isset($_POST['_schoolclass']) && !empty($_POST['_schoolclass']) && isset($_POST['studentIDs']) ){
+    $newKlass = $_POST['_schoolclass'];
+    $studentIDs = $_POST['studentIDs'];
 
-    foreach ($users as $user) {
-      if ( empty(get_user_meta($user, 'schoolclass', true)) ) {
-        add_user_meta($user, 'schoolclass', $newClass);
-      } elseif ($newClass > 0){
-        update_user_meta($user, 'schoolclass', $newClass);
-      } elseif ($newClass <= 0){
-        delete_user_meta($user, 'schoolclass');
+    // objectify the studentIDs
+    $students = array();
+    foreach ($studentIDs as $studentID) {
+      $students[] = new WP_User($studentID);
+    }
+
+    // We only accept changing to theese klasses in this function
+    $allowedKlasses = ['yearOne','yearTwo','yearThree','yearFour','yearFive','none'];
+    if (!in_array($newKlass, $allowedKlasses)){
+      echo "hehehra";
+      return false;
+    }
+
+    foreach ($students as $student) {
+      // if we are here we know that we want to remove this student from all currently set Klasses 
+      // because we only ever want a student to belong to one klass, and if 
+      // we want to uptade which klass that is we need to first remove his old klass
+      foreach ($allowedKlasses as $Klass) {
+        if (in_array($Klass, $student->caps)) {
+          $student->remove_cap($Klass);
+        }
+      }
+      // Add the student to the new klass if the user didnt 
+      // choose the "none" option
+      if ( $newKlass != "none" ) {
+        // now add him to the approved class cap
+        $student->add_cap($newKlass);
       }
     }
 
-  } 
+  }
 
-  // Output the form and table header ?>
+  printStudentTable();
+}
+function add_QlokastSchoolClass_to_admin_menu(){
+  add_users_page("School Classes","School Classes",'manage_options',"schoolclass", "schoolclass" );
+}
+add_action("admin_menu", "add_QlokastSchoolClass_to_admin_menu");
+
+// Output the form and table header 
+function printStudentTable(){
+?>
     <div class="wrap">
       <h1>Manage Student's "Class"<h1>
       <form method="post" action="">
@@ -61,14 +90,14 @@ function schoolclass(){
           </thead>
   <?php
 
-    // make a new row for every student
+  // make a new row for every student
   $students = get_users(array('role' => 'Student'));
   foreach ($students as $student) {
     ?>
       <tr id='<?php echo 'user-'.$student->ID ?>'>
         <th scope='row' class=''>
         <label class="screen-reader-text" for="<?php echo 'user_'.$student->ID ?>">Select <?php echo 'user_'.$student->nicename ?> </label>
-        <input type='checkbox' name='users[]' id='<?php echo 'user_'.$student->ID ?>' class='student' value='<?php echo $student->ID ?>' />
+        <input type='checkbox' name='studentIDs[]' id='<?php echo 'user_'.$student->ID ?>' class='student' value='<?php echo $student->ID ?>' />
         </th>
         <td class='username column-username has-row-actions column-primary' data-colname="Username">
           <strong>
@@ -76,7 +105,7 @@ function schoolclass(){
           </strong>
         </td>
         <td class='name column-name' data-colname="Name"> <?php echo $student->data->display_name ?> </td>
-        <td class='schoolclass column-roles' data-colname="SchoolClass"><?php echo $student->schoolclass ?></td>
+        <td class='schoolclass column-roles' data-colname="SchoolClass"><?php print_r($student->caps) ?></td>
       </tr>
     <?php
   }
@@ -88,12 +117,12 @@ function schoolclass(){
           <label for="bulk-action-selector-bottom" class="screen-reader-text">Select bulk action </label>
           <select name="_schoolclass" id="bulk-action-selector-bottom">
             <option value="">Change Class</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="-1">No Class</option>
+            <option value="yearOne">Year one</option>
+            <option value="yearTwo">Year two</option>
+            <option value="yearThree">Year three</option>
+            <option value="yearFour">Year four</option>
+            <option value="yearFive">Year five</option>
+            <option value="none">No Class</option>
           </select>
           <input type="submit" id="doaction2" class="button action" value="Apply"  />
         </div>
@@ -103,9 +132,4 @@ function schoolclass(){
 
   echo '</form></div>'; // end of wrap
 }
-
-function add_QlokastSchoolClass_to_admin_menu(){
-  add_users_page("School Classes","School Classes",'manage_options',"schoolclass", "schoolclass" );
-}
-add_action("admin_menu", "add_QlokastSchoolClass_to_admin_menu");
 
